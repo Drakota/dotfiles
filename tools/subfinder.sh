585407f073d6340c9b3fcf5431683a487a4cad56
Jonathan Bouchard <jonathaneutron@hotmail.com>
2020-12-07 23:48:07 -0500

    Added subfinder tool

---
+++ tools/subfinder.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+TOOL_NAME="subfinder"
+TOOL_VERSION="2.4.5"
+TOOL_ARCHIVE_PATH="/tmp/$TOOL_NAME.tar.gz"
+INSTALL_PATH=$HOME/.local/bin
+echo "[*] Installing $TOOL_NAME"
+
+wget "https://github.com/projectdiscovery/subfinder/releases/download/v${TOOL_VERSION}/subfinder_${TOOL_VERSION}_linux_amd64.tar.gz" -O $TOOL_ARCHIVE_PATH 
+echo "[*] Unpacking $TOOL_NAME..."
+tar -xzvf $TOOL_ARCHIVE_PATH -C /tmp 
+mv /tmp/$TOOL_NAME $INSTALL_PATH
+echo "[+] $TOOL_NAME successfully installed"
