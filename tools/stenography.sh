aa9f129a851dc39ad93a0aca60768e19ef4c4f8a
Jonathan Bouchard <jonathaneutron@hotmail.com>
2021-01-09 01:45:57 -0500

    Added stenography tools

---
+++ tools/stenography.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+TOOL_NAME="stenography tools"
+INSTALL_PATH=$HOME/.local/bin
+echo "[*] Installing $TOOL_NAME"
+
+DISTRIBUTION_ID="$(lsb_release -i)"
+if [[ $DISTRIBUTION_ID =~ "Kali" ]]; then
+	sudo apt update && sudo apt install exiftool steghide
+fi
+
+wget "http://www.caesum.com/handbook/Stegsolve.jar" -O $INSTALL_PATH/stegsolve.jar
+echo "[*] Adding stegsolve alias"
+echo -e "alias stegsolve='java -jar $INSTALL_PATH/stegsolve.jar'" >> $HOME/.bash_aliases
+source $HOME/.bashrc
+echo "[+] $TOOL_NAME successfully installed"
