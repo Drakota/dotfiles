92ba4de2812b5a73421f7d5d91b86eea3fcfa345
Jonathan Bouchard <jonathaneutron@hotmail.com>
2021-05-09 04:10:32 -0400

    Added logic2 tool

---
+++ tools/logic2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+DIR="$(dirname "$0")"
+source $DIR/../utils.sh
+
+TOOL_NAME="logic2"
+LOGIC2_APPIMAGE_URL="https://downloads.saleae.com/logic2/Logic-2.3.27-master.AppImage"
+
+preinstall_hook() { :; }
+
+install_hook() {
+    wget -q --show-progress $LOGIC2_APPIMAGE_URL -O /tmp/$TOOL_NAME
+    sudo chmod +x /tmp/$TOOL_NAME
+    mv /tmp/$TOOL_NAME $HOME/.local/bin/
+}
+
+postinstall_hook() { :; }
+
+setup_tool preinstall_hook install_hook postinstall_hook
