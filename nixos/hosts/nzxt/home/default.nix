cdf684ab818ea7c185d9614277039d3ce87d5241
Jonathan Bouchard <dev.drakota@gmail.com>
2025-05-19 21:09:42 -0400

    Added shell aliases

--- nixos/hosts/nzxt/home/default.nix
+++ nixos/hosts/nzxt/home/default.nix
@@ -18,6 +18,24 @@ in
     ".tmux.conf".source = config.lib.file.mkOutOfStoreSymlink tmuxConfPath;
   };
 
+  # Bash configuration
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      open = "xdg-open";
+      nrs = "sudo nixos-rebuild switch";
+      nrsu = "sudo nixos-rebuild switch --upgrade && nix store diff-closures /run/*-system";
+      nup = "sudo nix flake update --flake /etc/nixos";
+      ngc = "sudo nix-collect-garbage -d";
+      nxc = "$EDITOR /etc/nixos";
+    };
+    sessionVariables = {
+      # Set the default editor
+      EDITOR = "code";
+      VISUAL = "code";
+    };
+  };
+
   # This value determines the home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new home Manager release introduces backwards
